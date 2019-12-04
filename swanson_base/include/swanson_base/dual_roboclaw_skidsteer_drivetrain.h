9347996e204896d277927378627231163745cbd8
Hunter Young <hunter.lw.young@gmail.com>
2019-12-04 06:49:39 -0500

    updated dualclaw drivetrain node. successful dead reckoning nav. initial attempts to integrate robot_localization with updated nodes

--- swanson_base/include/swanson_base/dual_roboclaw_skidsteer_drivetrain.h
+++ swanson_base/include/swanson_base/dual_roboclaw_skidsteer_drivetrain.h
@@ -5,22 +5,41 @@
 #include <RoboCommander/drivetrains/dual_roboclaw.h>
 
 #include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
 #include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/Quaternion.h>
 #include <swanson_msgs/DualClawInfo.h>
+
+#include <tf/transform_broadcaster.h>
 
 using namespace std;
 
 class DualClawSkidsteerDrivetrainInterface{
 private:
      int pi;
+     int _count;
 
+     /** ROS Objects */
      ros::NodeHandle m_nh;
 	ros::NodeHandle p_nh;
      ros::Rate* _loop_rate;
-     int _count;
      ros::Subscriber cmd_sub;
      ros::Publisher data_pub;
+     ros::Publisher pose_pub;
+     ros::Publisher odom_pub;
+     tf::TransformBroadcaster _br;
 
+     /** ROS Params */
+     std::string _ns;
+     std::string _tf_prefix;
+     std::string _tf_odom;
+     std::string _tf_base;
+
+     /** Transforms */
+     tf::Transform _tfBaseToOdom;
+
+     /** ROS Subscriber Callbacks */
      void cmdCallback(const geometry_msgs::Twist::ConstPtr& msg, const int topic_index);
 
 public:
--- swanson_base/src/dual_roboclaw_skidsteer_drivetrain.cpp
+++ swanson_base/src/dual_roboclaw_skidsteer_drivetrain.cpp
@@ -7,39 +7,68 @@ using namespace std;
      CONSTRUCTOR & DECONSTRUCTOR
 */
 DualClawSkidsteerDrivetrainInterface::DualClawSkidsteerDrivetrainInterface(ros::NodeHandle nh, ros::NodeHandle _nh) : m_nh(nh), p_nh(_nh){
-
      // Declare constants
 	_count = 0;
+	_ns = m_nh.getNamespace();
 
+	/** Robot Hardware Interface */
 	std::string ser_dev = "/dev/ttyS0";
 	int ser_baud = 115200;
-	float max_turn_radius = 0.381;
-	float base_width = 0.219;
-     float max_speed = 2.0;
-	int qpps_per_meter = 9596;
-	float wheel_diameter = 0.1905;
-
-	std::string prefix;
-	std::string cmd_vel_topic = "cmd_vel";
-	std::string data_topic = "dualclaw/info";
 	int left_claw_addr = 128;
 	int right_claw_addr = 129;
-	float update_rate = 20;
-
 	p_nh.getParam("serial_device",ser_dev);
 	p_nh.getParam("serial_baud",ser_baud);
 	p_nh.getParam("left_claw_addr",left_claw_addr);
 	p_nh.getParam("right_claw_addr",right_claw_addr);
 
+	/** Robot Geometry Config */
+	float max_speed = 2.0;
+	float base_width = 0.219;
+	int qpps_per_meter = 9596;
+	float wheel_diameter = 0.1905;
+	float max_turn_radius = 0.381;
 	p_nh.getParam("max_speed",max_speed);
-	p_nh.getParam("max_turn_radius",max_turn_radius);
 	p_nh.getParam("base_width",base_width);
 	p_nh.getParam("qpps_per_meter",qpps_per_meter);
 	p_nh.getParam("wheel_diameter",wheel_diameter);
+	p_nh.getParam("max_turn_radius",max_turn_radius);
 
-	p_nh.getParam("prefix",prefix);
+	/** ROS Topic Config */
+	std::string cmd_vel_topic = "cmd_vel";
+	std::string data_topic = "dualclaw/info";
+	std::string pose_topic = "dualclaw/pose";
+	std::string odom_topic = "dead_reckoning";
 	p_nh.getParam("velocity_topic",cmd_vel_topic);
 	p_nh.getParam("data_topic",data_topic);
+	p_nh.getParam("pose_topic",pose_topic);
+	p_nh.getParam("odom_topic",odom_topic);
+
+	/** ROS tf Config */
+	bool flag_use_tf_prefix = true;
+	std::string tf_prefix = "";
+	std::string odom_frame = "odom";
+	std::string base_frame = "base_link";
+	p_nh.getParam("use_tf_prefix",flag_use_tf_prefix);
+	p_nh.getParam("tf_prefix",tf_prefix);
+	p_nh.getParam("odom_tf",odom_frame);
+	p_nh.getParam("base_tf",base_frame);
+
+	this->_tf_prefix = tf_prefix;
+	if(flag_use_tf_prefix){
+		this->_tf_odom = tf_prefix + odom_frame;
+		this->_tf_base = tf_prefix + base_frame;
+	} else{
+		this->_tf_odom = odom_frame;
+		this->_tf_base = base_frame;
+	}
+
+	/** Initialize tf transform */
+	tf::Vector3 pNull(0.0, 0.0, 0.0);
+	tf::Quaternion qNull; qNull.setRPY(0.0, 0.0, 0.0);
+	this->_tfBaseToOdom = tf::Transform(qNull,pNull);
+
+	/** ROS Misc Elements Config */
+	float update_rate = 20;
 	p_nh.getParam("update_rate",update_rate);
 
 	/* Connect to Pi. */
@@ -57,21 +86,25 @@ DualClawSkidsteerDrivetrainInterface::DualClawSkidsteerDrivetrainInterface(ros::
 		exit(0);
 	}
 
-	this->claws->set_base_width(base_width);
 	this->claws->set_max_speed(max_speed);
+	this->claws->set_base_width(base_width);
 	this->claws->set_qpps_per_meter(qpps_per_meter);
 	this->claws->set_wheel_diameter(wheel_diameter);
 
+	/** Initialize ROS Objects */
 	cmd_sub = m_nh.subscribe<geometry_msgs::Twist>(cmd_vel_topic, 50, boost::bind(&DualClawSkidsteerDrivetrainInterface::cmdCallback,this,_1,0));
-	data_pub = m_nh.advertise<swanson_msgs::DualClawInfo>(data_topic, 1000);
+	data_pub = m_nh.advertise<swanson_msgs::DualClawInfo>(data_topic, 10);
+	pose_pub = m_nh.advertise<geometry_msgs::PoseStamped>(pose_topic, 10);
+	odom_pub = m_nh.advertise<nav_msgs::Odometry>(odom_topic, 10);
 	_loop_rate = new ros::Rate(update_rate);
 	usleep(2 * 1000000);
 }
 
 DualClawSkidsteerDrivetrainInterface::~DualClawSkidsteerDrivetrainInterface(){
+	printf("[INFO] Shutting Down DualClawSkidsteerDrivetrainInterface...\r\n");
 	delete this->claws;
 	delete this->_loop_rate;
-	usleep(1 * 10000000);
+	// usleep(1 * 10000000);
 	pigpio_stop(this->pi);
 }
 
@@ -81,7 +114,6 @@ void DualClawSkidsteerDrivetrainInterface::cmdCallback(const geometry_msgs::Twis
 	// printf("[INFO] DualClawSkidsteerDrivetrainInterface::cmdCallback() ---- Recieved Cmds V,W: %.3f, %.3f\r\n",target_v,target_w);
 	claws->drive(target_v,target_w);
 }
-
 
 void DualClawSkidsteerDrivetrainInterface::update(bool verbose){
 	_count++;
@@ -94,11 +126,13 @@ void DualClawSkidsteerDrivetrainInterface::update(bool verbose){
      vector<float> spds = claws->get_encoder_speeds();
      vector<float> dOdom = claws->get_odom_deltas();
      vector<float> pose = claws->get_pose();
+     vector<float> vels = claws->get_velocities();
 
+	ros::Time curTime = ros::Time::now();
+	/** Update Roboclaw Data */
 	swanson_msgs::DualClawInfo dataMsg;
-	dataMsg.header.stamp = ros::Time::now();
+	dataMsg.header.stamp = curTime;
 	dataMsg.header.seq = _count;
-
 	dataMsg.left_roboclaw_voltage = voltages[0];
 	dataMsg.right_roboclaw_voltage = voltages[1];
 	dataMsg.qpps_per_meter = claws->get_qpps_per_meter();
@@ -117,20 +151,63 @@ void DualClawSkidsteerDrivetrainInterface::update(bool verbose){
 	dataMsg.left_motor_positions[1] = positions[1];
 	dataMsg.right_motor_positions[0] = positions[2];
 	dataMsg.right_motor_positions[1] = positions[3];
-
-	geometry_msgs::Pose2D poseMsg;
-	poseMsg.x = pose[0];
-	poseMsg.y = pose[1];
-	poseMsg.theta = pose[5];
-	dataMsg.estimated_pose = poseMsg;
-
+	dataMsg.estimated_pose.x = pose[0];
+	dataMsg.estimated_pose.y = pose[1];
+	dataMsg.estimated_pose.theta = pose[2];
 	data_pub.publish(dataMsg);
+
+	/** Update Robot's body transformations */
+	geometry_msgs::Quaternion quats = tf::createQuaternionMsgFromYaw(pose[2]);
+	// tf::Quaternion rot;
+	// tf::Vector3 trans(pose[0], pose[1], 0.0);
+	// tf::quaternionMsgToTF(quats, rot);
+	// this->_tfBaseToOdom = tf::Transform(rot,trans);
+	// this->_br.sendTransform(tf::StampedTransform(this->_tfBaseToOdom, curTime, this->_tf_base, this->_tf_odom));
+
+	geometry_msgs::TransformStamped odom_tf;
+	odom_tf.header = dataMsg.header;
+	odom_tf.header.frame_id = this->_tf_odom;
+	odom_tf.child_frame_id = this->_tf_base;
+
+	odom_tf.transform.translation.x = pose[0];
+	odom_tf.transform.translation.y = pose[1];
+	odom_tf.transform.translation.z = 0.0;
+	odom_tf.transform.rotation = quats;
+	this->_br.sendTransform(odom_tf);
+
+	/** Update Robot's dead-reckoned pose */
+	geometry_msgs::PoseStamped poseMsg;
+	poseMsg.header = odom_tf.header;
+	poseMsg.pose.position.x = pose[0];
+	poseMsg.pose.position.y = pose[1];
+	poseMsg.pose.position.z = 0.0;
+	poseMsg.pose.orientation = quats;
+	pose_pub.publish(poseMsg);
+
+	/** Update Robot's dead-reckoned odometry */
+	nav_msgs::Odometry odomMsg;
+	odomMsg.header = odom_tf.header;
+	odomMsg.child_frame_id = odom_tf.child_frame_id;
+
+	odomMsg.pose.pose = poseMsg.pose;
+	odomMsg.pose.covariance[0] = 0.01;
+	odomMsg.pose.covariance[7] = 0.01;
+	odomMsg.pose.covariance[14] = 99999;
+	odomMsg.pose.covariance[21] = 99999;
+	odomMsg.pose.covariance[28] = 99999;
+	odomMsg.pose.covariance[35] = 0.01;
+
+	odomMsg.twist.twist.linear.x = vels[0];
+	odomMsg.twist.twist.linear.y = 0.0;
+	odomMsg.twist.twist.angular.z = vels[1];
+	odomMsg.twist.covariance = odomMsg.pose.covariance;
+	odom_pub.publish(odomMsg);
 
 	if(verbose){
 		printf("Motor Speeds (m/s):  %.3f | %.3f  | %.3f  | %.3f \r\n",spds[0],spds[1],spds[2],spds[3]);
 		printf("Encoder Positions (qpps): %d | %d | %d | %d\r\n",positions[0],positions[1],positions[2],positions[3]);
-		printf("Δdistance, ΔX, ΔY, ΔYaw: %.3f, %.3f, %.3f, %.3f\r\n",dOdom[0], dOdom[1],dOdom[2],dOdom[3]);
-		printf("Current Pose [X (m), Y (m), Yaw (rad)]: %.3f     |    %.3f   |       %.3f\r\n",pose[0],pose[1],pose[5]);
+		printf("Δdistance, ΔYaw, ΔX, ΔY,: %.3f, %.3f, %.3f, %.3f\r\n",dOdom[0], dOdom[1],dOdom[2],dOdom[3]);
+		printf("Current Pose [X (m), Y (m), Yaw (rad)]: %.3f     |    %.3f   |       %.3f\r\n",pose[0],pose[1],pose[2]);
 		printf("Battery Voltages:     %.3f |    %.3f\r\n",voltages[0], voltages[1]);
 	     printf("Motor Currents:     %.3f |    %.3f |    %.3f |    %.3f\r\n",currents[0], currents[1], currents[2], currents[3]);
 		printf(" =========================================== \r\n");
@@ -142,8 +219,6 @@ int DualClawSkidsteerDrivetrainInterface::run(bool verbose){
 
      while(ros::ok()){
 		this->update(verbose);
-
-		// Update ROS index
 
           ros::spinOnce();
           _loop_rate->sleep();
